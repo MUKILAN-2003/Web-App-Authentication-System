7315852bd3bc0f99eee793c4e6b3851d8738a8b7
S.MUKILAN <65155327+MUKILAN-2003@users.noreply.github.com>
2021-06-27 21:53:26 +0530

    Updated

--- model/user.js
+++ model/user.js
@@ -3,6 +3,7 @@ const mongo = require('mongoose')
 const UserSchema = new mongo.Schema({
     username: { type: String, required: true, unique: true },
     name: { type: String, required: true, },
+    mail:{type:String,required:true},
     password: { type: String, required: true }
 }, { collection: 'Users' });
 
